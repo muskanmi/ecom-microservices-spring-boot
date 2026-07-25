39d7b6d84d06d2898e539385ffea0f7562b9aa90
Muskan Mittal <mittalmuskan758@gmail.com>
2026-07-25 11:56:26 +0530

    Initial project structure with 7 microservices scaffolded

---
+++ seller-admin-service/src/main/java/com/ecommerce/selleradminservice/SellerAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.selleradminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerAdminServiceApplication.class, args);
+	}
+
+}
