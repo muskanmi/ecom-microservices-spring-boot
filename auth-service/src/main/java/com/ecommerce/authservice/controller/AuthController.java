e682fea2373eb9caa5ba4c31e258e1b5dcf5604f
Muskan Mittal <mittalmuskan758@gmail.com>
2026-07-30 10:36:06 +0530

    added two fields in entity user

--- auth-service/src/main/java/com/ecommerce/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/ecommerce/authservice/controller/AuthController.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -97,4 +96,7 @@ public class AuthController {
 
         return ResponseEntity.ok(response);
     }
+
+    // Reset Passowrd
+    // Forgot Password
 }
