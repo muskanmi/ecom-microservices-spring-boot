39d29fffaf86c24866f182a5de8433744a5227c2
Muskan Mittal <mittalmuskan758@gmail.com>
2026-08-08 16:11:58 +0530

    created files and authapi implementation

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const API_URL = "Http://localhost:8081/api/auth";
+
+const authApi = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': "application/json"
+    }
+});
+
+export const registerUser = (data) => {
+    return authApi.post("/register", data);
+}
+
+export const loginUser = (data) => {
+    return authApi.post("/login", data);
+}
+
+export const currentUser = (token) => {
+    return authApi.get("/me", {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        }
+    })
+}
+
+export const updateProfile = (data, token) => {
+    return authApi.put("/me", data, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        }
+    })
+}
+
+export default authApi;
